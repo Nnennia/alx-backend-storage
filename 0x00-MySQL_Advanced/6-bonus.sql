b8b5903b14602d38a81f1607f1bbf11c017722ab
Nnennia <nnenniaatuanya@gmail.com>
2023-05-05 07:53:32 +0100

    commit

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,9 +1,18 @@
 -- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER //
 
-CREATE PROCEDURE AddBonus(user_id, )
+CREATE PROCEDURE AddBonus( IN user_id INT,
+    IN project_name VARCHAR(225),
+    IN score FLOAT)
 BEGIN
-	SELECT *  FROM products;
+    DECLARE project_id INT;
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
 END //
 
 DELIMITER ;
--- 0x00-MySQL_Advanced/README.md
+++ 0x00-MySQL_Advanced/README.md
@@ -1,1 +1,3 @@
 # MySQL Advanced
+## Using MySQL DELIMITER for stored procedures
+    Typically, a stored procedure contains multiple statements separated by semicolons (;). To compile the whole stored procedure as a single compound statement, you need to temporarily change the delimiter from the semicolon (;) to another delimiter such as $$ or //:
