7a70922395cf6ebc7b28e3ae50cd8bbf9e210d89
Nnennia <nnenniaatuanya@gmail.com>
2023-05-05 07:56:20 +0100

    commit

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,10 +5,9 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+    IN user_id INT)
 BEGIN
-    DECLARE average_score FLOAT
+    DECLARE average_score FLOAT;
     SET average_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
     UPDATE users SET average_score = average_score WHERE id=user_id;
 END //
