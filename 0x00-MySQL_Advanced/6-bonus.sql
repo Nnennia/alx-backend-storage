8cc91b7a5996d5c87f0e799b3067cf01fa25174a
Nnennia <nnenniaatuanya@gmail.com>
2023-05-05 07:19:21 +0100

    commit

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,9 @@
+-- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(user_id, )
+BEGIN
+	SELECT *  FROM products;
+END //
+
+DELIMITER ;
