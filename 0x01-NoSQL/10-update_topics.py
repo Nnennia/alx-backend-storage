b186aa215002f0ab712f620b6fa396078ad54c20
Nnennia <nnenniaatuanya@gmail.com>
2023-05-08 18:41:15 +0100

    changes to 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """A python function that changes all topics of a school document based on the name"""
-from pymongo.collection import Collection
 from typing import List
 
-def update_topics(mongo_collection: Collection, name: str, topics: List[str]):
+def update_topics(mongo_collection, name: str, topics: List[str]):
     """
     Updates the topics of a school document based on the name.
 
